0ff8f338c01097e976f83cd6abc7ae997a32b038
Bruce Eckel <BruceTEckel@gmail.com>
2023-05-17 14:17:22 -0600

    Basic string tests

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,4 +1,15 @@
+// The only use case for Box<str> over String is optimising for
+// memory usage when the string is fixed and you do not intend
+// to append or remove from it.
+
+// format!() seems more expedient than String::from()
+
 fn main() {
-    let b = Box::new("Hello, world!");
-    println!("{b}");
+    let _w = "world!";
+    let _b = Box::new("Hello, ".to_owned() + _w);  // Can't do any formatting with {}
+    println!("{_b}");
+    let g = String::from("Goodbye, ".to_owned() + _w);  // Can't do any formatting with {}
+    println!("{g}");
+    let f = format!("{} ...Goodbye...", g); // Can do all the formatting you want...
+    println!("{f}");
 }
