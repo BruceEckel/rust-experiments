b0ab4eb2090bbd255f12ff647996ca258316d00b
Bruce Eckel <BruceTEckel@gmail.com>
2023-05-19 10:32:14 -0600

    Update main.rs

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,15 +1,27 @@
+// format!() seems more expedient than String::from()
+
 // The only use case for Box<str> over String is optimising for
 // memory usage when the string is fixed and you do not intend
 // to append or remove from it.
 
-// format!() seems more expedient than String::from()
-
 fn main() {
     let w = "world!";
-    let b = Box::new("Hello, ".to_owned() + w);  // Can't do any formatting with {}
-    println!("{b}");
-    let g = String::from("Goodbye, ".to_owned() + w);  // Can't do any formatting with {}
-    println!("{g}");
-    let f = format!("{} ...Goodbye...", g); // Can do all the formatting you want...
+    let hw = Box::new("Hello, ".to_owned() + w);  // Can't do any formatting with {}
+    println!("{hw}");
+    let gb = "Goodbye ";
+    let gw = String::from("Alas, ".to_owned() + gb + w);  // Can't do any formatting with {}
+    println!("{gw}");
+    let f = format!("{hw}, ...Goodbye... {}.", gw); // Can do all the formatting you want...
     println!("{f}");
+
+    let mut sn = String::new();
+    sn.push('X');
+    sn.push_str("foo");
+    sn.push_str("bar");
+    println!("{sn}");
+
+    // https://www.poetryfoundation.org/poems/46565/ozymandias
+    for w in "Look on my Works, ye Mighty, and despair!".split_whitespace() {
+        print!("{w} ");
+    }
 }
