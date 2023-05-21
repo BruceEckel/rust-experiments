cfb1281f5df31fae8b7a0f73f6d5f2342db392d8
Bruce Eckel <BruceTEckel@gmail.com>
2023-05-21 16:19:23 -0600

    shapes

--- shapes/src/main.rs
+++ shapes/src/main.rs
@@ -1,5 +1,4 @@
 // Polymorphism: dyn vs. impl
-#![allow(dead_code, unused_variables)]
 
 trait Shape {
     fn draw(&self);
@@ -27,18 +26,18 @@ impl Shape for Rectangle {
     }
 }
 
-// `&` because size is unknown, `dyn` because type is unknown.
-// `dyn` produces a "fat pointer" including a vtable.
-fn show_dyn(s: &dyn Shape) {
-    s.draw();
-}
-// Shape isn't automatically burdened with a vtable!
-
 // Like C++ templates--new version generated for each argument type:
 fn show_impl(s: impl Shape) {
     s.draw();
 }
 // Invisible automatically-generated function overloading!
+
+// `&` because size is unknown, `dyn` because type is unknown.
+// `dyn` produces a "fat pointer" including a vtable.
+fn show_dyn(s: &dyn Shape) {
+    s.draw();
+}
+// Shape isn't automatically burdened with a vtable! ("zero-cost abstractions")
 
 fn main() {
     const SHAPES: [&dyn Shape; 2] = [
@@ -47,11 +46,7 @@ fn main() {
     ];
     SHAPES[0].draw();
     SHAPES[1].draw();
-    for shape in SHAPES {
-        show_dyn(shape);
-        // Doesn't know the type at compile time, so it can't generate:
-        // show_impl(shape);
-    }
+
     let (c, r) = (
         Circle { radius: 4 },
         Rectangle { length: 5, width: 6, },
@@ -60,19 +55,23 @@ fn main() {
     show_impl(r); // Creates a version of show_impl() for Square
     // show_impl(*SHAPES[0]);  // Doesn't know the type
 
+    for shape in SHAPES {
+        show_dyn(shape);
+        // Doesn't know the type at compile time:
+        // show_impl(shape);
+    }
     vec_of_shapes();
 }
 
 fn vec_of_shapes() {
-    // Rust can't infer the type here:
-    let shapes: Vec<Box<&dyn Shape>> = vec![
-        Box::new(&Circle { radius: 11 }),
-        Box::new(&Rectangle { length: 12, width: 13 }),
-        Box::new(&Circle { radius: 14 }),
-        Box::new(&Rectangle { length: 15, width: 16 }),
+    // Rust can't infer the type:
+    let shapes: Vec<&dyn Shape> = vec![
+        &Circle { radius: 11 },
+        &Rectangle { length: 12, width: 13 },
+        &Circle { radius: 14 },
+        &Rectangle { length: 15, width: 16 },
     ];
-
     for shape in shapes.iter() {
-        show_dyn(**shape);
+        show_dyn(*shape);
     }
 }
