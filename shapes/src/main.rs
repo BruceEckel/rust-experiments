44845e313c2ca125c3e4bd10a1b892f4e61be462
Bruce Eckel <BruceTEckel@gmail.com>
2023-05-22 10:49:47 -0600

    Added impl_variations()

--- shapes/src/main.rs
+++ shapes/src/main.rs
@@ -1,4 +1,5 @@
 // Polymorphism: dyn vs. impl
+use std::ops::Deref;
 
 trait Shape {
     fn draw(&self);
@@ -61,6 +62,7 @@ fn main() {
         // show_impl(shape);
     }
     vec_of_shapes();
+    impl_variations();
 }
 
 fn vec_of_shapes() {
@@ -74,4 +76,37 @@ fn vec_of_shapes() {
     for shape in shapes.iter() {
         show_dyn(*shape);
     }
+}
+
+fn impl_variations() {  // Thanks to @mp
+    impl Shape for Box<dyn Shape> {
+        fn draw(&self) {
+            print!("Box<dyn Shape> draw: ");
+            self.deref().draw()
+        }
+    }
+
+    // This isn't matching for some reason. @mp didn't know, but
+    // said "never seen a &dyn Trait in practice — only Box<dyn Trait>"
+    impl Shape for &dyn Shape {
+        fn draw(&self) {
+            print!("&dyn Shape draw: ");
+            self.deref().draw()
+        }
+    }
+
+    let shapes1: [Box<dyn Shape>; 2] = [
+        Box::new(Circle { radius: 99 }),
+        Box::new(Rectangle { length: 100, width: 101, }),
+    ];
+
+    for shape in shapes1 { shape.draw(); }
+
+    let shapes2: [&dyn Shape; 2] = [
+        &Circle { radius: 200 },
+        &Rectangle { length: 201, width: 202, },
+    ];
+
+    // Doesn't match the expected impl:
+    for shape in shapes2 { shape.draw(); }
 }
