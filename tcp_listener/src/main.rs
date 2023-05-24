15cd97b81e5d541f33dd5135a6a5927cb45d0a44
Bruce Eckel <BruceTEckel@gmail.com>
2023-05-23 22:04:30 -0600

    tcp_listener

---
+++ tcp_listener/src/main.rs
@@ -0,0 +1,38 @@
+// From https://google.github.io/comprehensive-rust/async/tasks.html
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6142").await?;
+    println!("listening on port 6142");
+
+    loop {
+        let (mut socket, addr) = listener.accept().await?;
+
+        println!("connection from {addr:?}");
+
+        tokio::spawn(async move {
+            if let Err(e) = socket.write_all(b"Who are you?\n").await {
+                println!("socket error: {e:?}");
+                return;
+            }
+
+            let mut buf = vec![0; 1024];
+            let reply = match socket.read(&mut buf).await {
+                Ok(n) => {
+                    let name = std::str::from_utf8(&buf[..n]).unwrap().trim();
+                    format!("Thanks for dialing in, {name}!\n")
+                }
+                Err(e) => {
+                    println!("socket error: {e:?}");
+                    return;
+                }
+            };
+
+            if let Err(e) = socket.write_all(reply.as_bytes()).await {
+                println!("socket error: {e:?}");
+            }
+        });
+    }
+}
