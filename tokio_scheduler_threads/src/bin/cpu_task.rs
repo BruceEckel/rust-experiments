45ea47303c92e2839c2700bc9d857ef84ea8eef2
Bruce Eckel <MindviewInc@gmail.com>
2023-09-22 15:26:12 -0600

    changing to function-passing approach
    (checkpoint with current working code)

--- tokio_scheduler_threads/src/bin/cpu_task.rs
+++ tokio_scheduler_threads/src/bin/cpu_task.rs
@@ -4,15 +4,23 @@ use std::thread;
 use std::time::Instant;
 use tokio::runtime::Runtime;
 
-pub async fn rand_int(rng: &mut StdRng) -> i32 {
+pub async fn rand_int_yield(rng: &mut StdRng) -> i32 {
     tokio::task::yield_now().await; // Allow context switch
     rng.gen_range(0..100)
 }
 
-pub async fn calculation(name: &str) {
+pub async fn rand_int_no_yield(rng: &mut StdRng) -> i32 {
+    // No context switch
+    rng.gen_range(0..100)
+}
+
+pub async fn calculation<F>(name: &str, mut rand_func: F)
+where
+    F: FnMut(&mut StdRng) -> std::pin::Pin<Box<dyn std::future::Future<Output = i32> + Send>>,
+{
     let current_thread = thread::current();
     println!(
-        "Task {} starts on thread: {:?} (id: {:?})",
+        "Task {} starts on thread {:?} (id: {:?})",
         name,
         current_thread.name().unwrap_or("[unnamed]"),
         current_thread.id()
@@ -22,7 +30,7 @@ pub async fn calculation(name: &str) {
 
     let mut x = 0;
     for _ in 0..1_000_000 {
-        x += rand_int(&mut rng).await;
+        x += (rand_func(&mut rng)).await;
     }
 
     println!("Task {} ends after {:?}: {}", name, start.elapsed(), x);
@@ -32,8 +40,16 @@ pub fn run_tasks(rt: Runtime) {
     rt.block_on(async {
         let start = Instant::now();
 
-        let task_one = tokio::spawn(calculation("one"));
-        let task_two = tokio::spawn(calculation("two"));
+        let task_one = tokio::spawn(calculation("one", |rng| {
+            let mut cloned_rng = rng.clone();
+            let future = async move { rand_int_yield(&mut cloned_rng).await };
+            Box::pin(future)
+        }));
+        let task_two = tokio::spawn(calculation("two", |rng| {
+            let mut cloned_rng = rng.clone();
+            let future = async move { rand_int_no_yield(&mut cloned_rng).await };
+            Box::pin(future)
+        }));
 
         // Await the completion of both tasks
         let _ = tokio::try_join!(task_one, task_two);
