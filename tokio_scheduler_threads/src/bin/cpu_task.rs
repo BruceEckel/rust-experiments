b65f155480bbfee4d5e6f2f9424be5ff73fd5958
Bruce Eckel <mindviewinc@gmail.com>
2023-09-24 13:29:05 -0600

    reorging

--- tokio_scheduler_threads/src/bin/cpu_task.rs
+++ tokio_scheduler_threads/src/bin/cpu_task.rs
@@ -1,7 +1,6 @@
 use std::thread;
 use std::time::Instant;
 
-<<<<<<< HEAD
 pub async fn rand_int(rng: &mut StdRng, yielding: bool) -> i32 {
     if yielding {
         tokio::task::yield_now().await; // Allow context switch
@@ -9,28 +8,19 @@ pub async fn rand_int(rng: &mut StdRng, yielding: bool) -> i32 {
     rng.gen_range(0..100)
 }
 
-pub async fn calculation<F>(name: &str, mut rand_func: F)
-where
-    F: FnMut(&mut StdRng) -> std::pin::Pin<Box<dyn std::future::Future<Output = i32> + Send>>,
-{
+pub async fn calculation(name: &str, yielding: bool) {
     let current_thread = thread::current();
     println!(
-        "Task {} starts on thread {:?} (id: {:?})",
+        "{} starts on thread {:?} (id: {:?})",
         name,
         current_thread.name().unwrap_or("[unnamed]"),
         current_thread.id()
     );
-=======
-pub fn cpu_bound_task(name: &str, iterations: usize) {
-    let current_thread = thread::current();
-    println!("{} starts on thread: {:?} (id: {:?})", name, current_thread.name().unwrap_or("[unnamed]"), current_thread.id());
->>>>>>> parent of 471892c (random generator with yield, run_tasks())
     let start = Instant::now();
 
     let mut x = 0;
-<<<<<<< HEAD
     for _ in 0..1_000_000 {
-        x += (rand_func(&mut rng)).await;
+        x += (rand_int(&mut rng, yielding)).await;
     }
 
     println!("Task {} ends after {:?}: {}", name, start.elapsed(), x);
@@ -40,27 +30,13 @@ pub fn run_tasks(rt: Runtime) {
     rt.block_on(async {
         let start = Instant::now();
 
-        let task_one = tokio::spawn(calculation("one", |rng| {
-            let mut cloned_rng = rng.clone();
-            let future = async move { rand_int(&mut cloned_rng, true).await };
-            Box::pin(future)
-        }));
-        let task_two = tokio::spawn(calculation("two", |rng| {
-            let mut cloned_rng = rng.clone();
-            let future = async move { rand_int(&mut cloned_rng, true).await };
-            Box::pin(future)
-        }));
+        let task_one = tokio::spawn(calculation("one", true));
+        let task_two = tokio::spawn(calculation("two", true));
 
         // Await the completion of both tasks
         let _ = tokio::try_join!(task_one, task_two);
 
         println!("Duration: {:?}", start.elapsed());
     });
-=======
-    for _ in 0..iterations {
-        x += 1;
-    }
-
     println!("{} ends after {:?}: {}", name, start.elapsed(), x);
->>>>>>> parent of 471892c (random generator with yield, run_tasks())
 }
--- tokio_scheduler_threads/src/bin/multi_threaded.rs
+++ tokio_scheduler_threads/src/bin/multi_threaded.rs
@@ -1,13 +1,13 @@
-use tokio::task;
 use std::time::Instant;
+use tokio::task;
 mod cpu_task;
-use cpu_task::cpu_bound_task;  // Using the function
+use cpu_task::calculation; // Using the function
 
 #[tokio::main] // No flavor attribute: multi-threaded
 async fn main() {
     let start = Instant::now();
-    let t1 = task::spawn_blocking(|| cpu_bound_task("Task one", 1_000_000_000));
-    let t2 = task::spawn_blocking(|| cpu_bound_task("Task two", 1_000_000_000));
+    let t1 = task::spawn_blocking(|| calculation("Task one", 1_000_000_000));
+    let t2 = task::spawn_blocking(|| calculation("Task two", 1_000_000_000));
 
     let _ = tokio::join!(t1, t2);
     println!("Duration: {:?}", start.elapsed());
