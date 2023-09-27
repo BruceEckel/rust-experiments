0838d32de5d974809a93bb5a3b567c575d8d734c
Bruce Eckel <MindviewInc@gmail.com>
2023-09-27 09:45:14 -0600

    Configured rust formatting

--- tokio_scheduler_threads/src/main.rs
+++ tokio_scheduler_threads/src/main.rs
@@ -5,15 +5,25 @@ use std::time::Instant;
 use tokio::runtime::Builder;
 use tokio::runtime::Runtime;
 
-pub async fn rand_int(rng: &mut StdRng, yielding: bool) -> i32 {
+pub async fn rand_int(
+    rng: &mut StdRng,
+    yielding: bool,
+) -> i32 {
     if yielding {
-        tokio::task::yield_now().await; // Allow context switch
+        // Allow context switch
+        tokio::task::yield_now().await;
     }
     rng.gen_range(0..100)
 }
 
-pub async fn calculation(name: &str, yielding: bool) {
-    println!("\nStarting '{}' with yielding {}", name, yielding);
+pub async fn calculation(
+    name: &str,
+    yielding: bool,
+) {
+    println!(
+        "\nStarting '{}' with yielding {}",
+        name, yielding
+    );
     let current_thread = thread::current();
     println!(
         "'{}' starts on thread {:?} (id: {:?})",
@@ -22,38 +32,60 @@ pub async fn calculation(name: &str, yielding: bool) {
         current_thread.id()
     );
     let start = Instant::now();
-    let mut rng: StdRng = SeedableRng::from_seed([42; 32]);
+    let mut rng: StdRng =
+        SeedableRng::from_seed([42; 32]);
 
     let mut x = 0;
     for _ in 0..1_000_000 {
-        x += (rand_int(&mut rng, yielding)).await;
+        x += (rand_int(&mut rng, yielding))
+            .await;
     }
 
-    println!("Task '{}' ends after {:?}: {}", name, start.elapsed(), x);
+    println!(
+        "Task '{}' ends after {:?}: {}",
+        name,
+        start.elapsed(),
+        x
+    );
 }
 
-pub fn run_tasks(rt: &Runtime, yielding: bool) {
+pub fn run_tasks(
+    rt: &Runtime,
+    yielding: bool,
+) {
     rt.block_on(async {
         let start = Instant::now();
-        let task_one = tokio::spawn(calculation("one", yielding));
-        let task_two = tokio::spawn(calculation("two", yielding));
-        let _ = tokio::try_join!(task_one, task_two);
-        println!("Duration: {:?}", start.elapsed());
+        let task_one = tokio::spawn(
+            calculation("one", yielding),
+        );
+        let task_two = tokio::spawn(
+            calculation("two", yielding),
+        );
+        let _ = tokio::try_join!(
+            task_one, task_two
+        );
+        println!(
+            "Duration: {:?}",
+            start.elapsed()
+        );
     });
 }
 
 fn main() {
     println!("Single-threaded tokio async");
-    let rt = Builder::new_current_thread().enable_all().build().unwrap();
-    run_tasks(&rt, true);
-    run_tasks(&rt, false);
+    let rts = Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    run_tasks(&rts, true);
+    run_tasks(&rts, false);
 
     println!("\nMulti-threaded tokio async");
-    let rt = Builder::new_multi_thread()
+    let rtm = Builder::new_multi_thread()
         .enable_all()
         .worker_threads(2)
         .build()
         .unwrap();
-    run_tasks(&rt, true);
-    run_tasks(&rt, false);
+    run_tasks(&rtm, true);
+    run_tasks(&rtm, false);
 }
