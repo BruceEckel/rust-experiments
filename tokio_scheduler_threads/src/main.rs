8e0105feae1e9f30075a7f3651ccc9b58088af5b
Bruce Eckel <MindviewInc@gmail.com>
2023-09-28 10:27:43 -0600

    Added variable yielding

--- tokio_scheduler_threads/src/main.rs
+++ tokio_scheduler_threads/src/main.rs
@@ -1,28 +1,42 @@
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
+use std::sync::Arc;
 use std::thread;
 use std::time::Instant;
-use tokio::runtime::Builder;
-use tokio::runtime::Runtime;
+use tokio::runtime::{Builder, Runtime};
+
+#[derive(Copy, Clone)]
+pub struct YieldPercent {
+    pub value: i32,
+}
+
+impl YieldPercent {
+    pub fn new(value: i32) -> Self {
+        Self {
+            value: value.clamp(0, 100),
+        }
+    }
+}
 
 pub async fn rand_int(
     rng: &mut StdRng,
-    yielding: bool,
+    yield_percent: &YieldPercent,
 ) -> i32 {
-    if yielding {
-        // Allow context switch
+    let random = rng.gen_range(0..100);
+    if random < yield_percent.value {
+        // Probability-based context switch
         tokio::task::yield_now().await;
     }
-    rng.gen_range(0..100)
+    random
 }
 
 pub async fn calculation(
     name: &str,
-    yielding: bool,
+    yield_percent: Arc<YieldPercent>,
 ) {
     println!(
-        "\nStarting '{}' with yielding {}",
-        name, yielding
+        "\nStart '{}' with yield_percent {}",
+        name, yield_percent.value
     );
     let current_thread = thread::current();
     println!(
@@ -36,8 +50,11 @@ pub async fn calculation(
         SeedableRng::from_seed([42; 32]);
     let mut x = 0;
     for _ in 0..1_000_000 {
-        x += (rand_int(&mut rng, yielding))
-            .await;
+        x += rand_int(
+            &mut rng,
+            &*yield_percent,
+        )
+        .await;
     }
     println!(
         "Task '{}' ends after {:?}: {}",
@@ -49,38 +66,55 @@ pub async fn calculation(
 
 pub fn run_tasks(
     rt: &Runtime,
-    yielding: bool,
+    yield_percent: YieldPercent,
 ) {
     let start = Instant::now();
+    let yield_percent =
+        Arc::new(yield_percent);
     rt.block_on(async {
-        let task_one = tokio::spawn(
-            calculation("one", yielding),
-        );
-        let task_two = tokio::spawn(
-            calculation("two", yielding),
-        );
+        let task_one =
+            tokio::spawn(calculation(
+                "one",
+                yield_percent.clone(),
+            ));
+        let task_two =
+            tokio::spawn(calculation(
+                "two",
+                yield_percent.clone(),
+            ));
         let _ = tokio::try_join!(
             task_one, task_two
         );
     });
-    println!("=> Elapsed: {:?}", start.elapsed());
+    println!(
+        "=> Elapsed: {:?}",
+        start.elapsed()
+    );
 }
 
 fn main() {
+    let yields = [
+        YieldPercent::new(0),
+        YieldPercent::new(5),
+        YieldPercent::new(50),
+        YieldPercent::new(100),
+    ];
     println!("Single-threaded tokio async");
     let rts = Builder::new_current_thread()
         .enable_all()
         .build()
         .unwrap();
-    run_tasks(&rts, false);
-    run_tasks(&rts, true);
+    for yld in &yields {
+        run_tasks(&rts, *yld);
+    }
 
-    println!("\nMulti-threaded tokio async");
+    println!("\nTwo-threaded tokio async");
     let rtm = Builder::new_multi_thread()
         .enable_all()
         .worker_threads(2)
         .build()
         .unwrap();
-    run_tasks(&rtm, false);
-    run_tasks(&rtm, true);
+    for yld in &yields {
+        run_tasks(&rtm, *yld);
+    }
 }
