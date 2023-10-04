4319bc0272576ce406e6b5c0043188402eb9eeef
Bruce Eckel <MindviewInc@gmail.com>
2023-10-04 12:16:50 -0600

    tuning

--- concurrent_failure/multiple_tasks/src/main.rs
+++ concurrent_failure/multiple_tasks/src/main.rs
@@ -1,21 +1,14 @@
-// Demonstrate different failures in tasks
-
 #[tokio::main]
 async fn main() {
     let tasks: Vec<_> = (0..10)
         .map(|i| {
             tokio::spawn(async move {
-                if i == 5 {
-                    panic!("i:{} panicked!", i);
-                } else if i % 2 == 0 {
-                    Err::<char, String>(
-                        format!("Failed({})", i)
-                    )
-                } else {
-                    // Convert number to letter:
-                    let letter =
-                        (b'a' + i as u8) as char;
-                    Ok::<char, String>(letter)
+                match i {
+                    5 => panic!("i:{} panicked!", i),
+                    _ if i % 2 == 0 => {
+                        Err(format!("Failed({})", i))
+                    }
+                    _ => Ok((b'a' + i as u8) as char),
                 }
             })
         })
@@ -29,7 +22,7 @@ async fn main() {
         match result {
             Ok(Ok(l)) => println!("Letter: {}", l),
             Ok(Err(e)) => println!("Err: {}", e),
-            Err(p) => println!("Panic: {}", p),
+            Err(p) => println!("Panic: {:?}", p),
         }
     }
 }
