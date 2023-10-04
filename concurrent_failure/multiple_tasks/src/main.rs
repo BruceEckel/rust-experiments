bf564ee4ceadc46f0e886969156539085c0d282f
Bruce Eckel <MindviewInc@gmail.com>
2023-10-04 11:44:31 -0600

    Tweak formatting

--- concurrent_failure/multiple_tasks/src/main.rs
+++ concurrent_failure/multiple_tasks/src/main.rs
@@ -1,26 +1,34 @@
+// Demonstrate different failures in tasks
+
 #[tokio::main]
 async fn main() {
-    let tasks: Vec<_> = (0..10).map(|i| {
-        tokio::spawn(async move {
-            if i == 5 {
-                panic!("i:{} panicked!", i);
-            } else if i % 2 == 0 { // Demonstrate failure:
-                Err::<char, String>(format!("Failed({})", i))
-            } else {
-                // Convert number to letter:
-                let letter = (b'a' + i as u8) as char;
-                Ok::<char, String>(letter)
-            }
+    let tasks: Vec<_> = (0..10)
+        .map(|i| {
+            tokio::spawn(async move {
+                if i == 5 {
+                    panic!("i:{} panicked!", i);
+                } else if i % 2 == 0 {
+                    Err::<char, String>(
+                        format!("Failed({})", i)
+                    )
+                } else {
+                    // Convert number to letter:
+                    let letter =
+                        (b'a' + i as u8) as char;
+                    Ok::<char, String>(letter)
+                }
+            })
         })
-    }).collect();
+        .collect();
 
-    let results: Vec<_> = futures::future::join_all(tasks).await;
+    let results: Vec<_> =
+        futures::future::join_all(tasks).await;
 
     for result in results.iter() {
         print!("{:?} => ", result);
         match result {
-            Ok(Ok(letter)) => println!("Letter: {}", letter),
-            Ok(Err(e)) => println!("Error: {}", e),
+            Ok(Ok(l)) => println!("Letter: {}", l),
+            Ok(Err(e)) => println!("Err: {}", e),
             Err(p) => println!("Panic: {}", p),
         }
     }
