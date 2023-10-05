8ef68f07c0ddd75896db8174d141146622d17876
Bruce Eckel <MindviewInc@gmail.com>
2023-10-05 11:22:45 -0600

    Custom panic handler

--- concurrent_failure/multiple_tasks/src/main.rs
+++ concurrent_failure/multiple_tasks/src/main.rs
@@ -1,34 +1,42 @@
-use std::sync::Arc;
-use tokio::sync::Mutex;
+use std::panic;
+use std::sync::{Arc, Mutex as StdMutex};
+use tokio::sync::Mutex as AsyncMutex;
 
 async fn fallible(
     i: usize,
-    print_lock: Arc<Mutex<()>>,
+    print_lock: Arc<AsyncMutex<()>>,
 ) -> Result<char, String> {
     {
-        // Can't write to output simultaneously:
         let _lock = print_lock.lock().await;
         println!("fallible({})", i);
-    } // _lock released
+    }
 
     match i {
         5 => panic!("i:{} panicked!", i),
         _ if i % 2 == 0 => {
             Err(format!("Failed({})", i))
         }
-        // Convert number to char:
         _ => Ok((b'a' + i as u8) as char),
     }
 }
 
 #[tokio::main]
 async fn main() {
-    // Prevents interleaving output:
-    let print_lock = Arc::new(Mutex::new(()));
+    let print_lock = Arc::new(AsyncMutex::new(()));
+    let panic_lock = Arc::new(StdMutex::new(()));
+
+    let plock = panic_lock.clone();
+    panic::set_hook(Box::new(move |info| {
+        let _lock = plock.lock().unwrap();
+        println!("Custom panic: {}", info);
+    }));
 
     let tasks: Vec<_> = (0..10)
         .map(|i| {
-            tokio::spawn(fallible(i, print_lock.clone()))
+            tokio::spawn(fallible(
+                i,
+                print_lock.clone(),
+            ))
         })
         .collect();
 
@@ -39,7 +47,6 @@ async fn main() {
 
     let results: Vec<_> =
         futures::future::join_all(tasks).await;
-
     for result in results.iter() {
         let _lock = print_lock.lock().await;
         print!("{:?} => ", result);
