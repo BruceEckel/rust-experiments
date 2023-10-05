4bb580b55c0b4b08b452ec03016f11e580daf0d6
Bruce Eckel <MindviewInc@gmail.com>
2023-10-05 10:51:19 -0600

    Added print_lock

--- concurrent_failure/multiple_tasks/src/main.rs
+++ concurrent_failure/multiple_tasks/src/main.rs
@@ -1,30 +1,48 @@
-use std::io::{self, Write};
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
-async fn fallible(i: usize) -> Result<char, String> {
-    println!("fallible({})", i);
-    io::stdout().flush().unwrap();
+async fn fallible(
+    i: usize,
+    print_lock: Arc<Mutex<()>>,
+) -> Result<char, String> {
+    {
+        // Can't write to output simultaneously:
+        let _lock = print_lock.lock().await;
+        println!("fallible({})", i);
+    } // _lock released
+
     match i {
         5 => panic!("i:{} panicked!", i),
         _ if i % 2 == 0 => {
             Err(format!("Failed({})", i))
         }
+        // Convert number to char:
         _ => Ok((b'a' + i as u8) as char),
     }
 }
 
 #[tokio::main]
 async fn main() {
+    // Prevents interleaving output:
+    let print_lock = Arc::new(Mutex::new(()));
+
     let tasks: Vec<_> = (0..10)
-        .map(|i| tokio::spawn(fallible(i)))
+        .map(|i| {
+            tokio::spawn(fallible(i, print_lock.clone()))
+        })
         .collect();
-    println!("Tasks created");
-    io::stdout().flush().unwrap();
+
+    {
+        let _lock = print_lock.lock().await;
+        println!("Tasks created");
+    }
 
     let results: Vec<_> =
         futures::future::join_all(tasks).await;
+
     for result in results.iter() {
+        let _lock = print_lock.lock().await;
         print!("{:?} => ", result);
-        io::stdout().flush().unwrap();
         match result {
             Ok(Ok(l)) => println!("Letter: {}", l),
             Ok(Err(e)) => println!("Err: {}", e),
